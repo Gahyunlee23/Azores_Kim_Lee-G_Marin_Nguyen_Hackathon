c27b2446d9b720a849bec03acfdede0c0f3433f2
Dana Marin <d_maringarcia@fanshaweonline.ca>
2020-02-11 06:32:02 -0500

    changing gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 
 function compile(done) {
-    gulp.src('./sass/*/.scss')
+    gulp.src('./sass/**/*.scss')
     .pipe(sass())
     .on('error', sass.logError)
     .pipe(gulp.dest('./css'))
